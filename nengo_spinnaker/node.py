131c53385d6eafbf181fc8d1fcf161c9fddf6fb1
AndrewMundy <andrew.mundy@ieee.org>
2015-04-01 13:34:50 +0100

    Build IO for Nodes.
    A `NodeIOController` can be used to:
     - (1) build the SpiNNaker side of NodeIO
     - (2) build the host side of NodeIO
     - (3) provide the network that is to be simulated on the PC

---
+++ nengo_spinnaker/node.py
@@ -0,0 +1,168 @@
+import nengo
+
+from .intermediate_representation import IntermediateObject, IntermediateNet
+from . import netlist as nl
+
+
+class IntermediateHostNode(IntermediateObject):
+    """Intermediate object representing a Node which will be simulated on the
+    host and consequently will not directly result in a SpiNNaker executable.
+    """
+
+
+class NodeIOController(object):
+    """Object which controls communication between SpiNNaker and a host
+    computer for transmitting output and receiving input for Node objects.
+
+    The standard NodeIOController should not be used, but rather one of its
+    subclasses.  Subclasses are required to provide methods to fill in the
+    SpiNNaker side of Node simulation; these methods are:
+
+        - `get_spinnaker_source_for_node`
+        - `get_spinnaker_sink_for_node`
+
+    A `NodeIOController` can be used during the construction of an intermediate
+    representation using the `extra_object_builders`, `extra_source_getters`,
+    `extra_sink_getters` and `extra_probe_builders` arguments.
+
+    The following two methods must also be implemented:
+
+        - `get_node_input`
+        - `set_node_output`
+    """
+    def __init__(self):
+        # Create an empty Nengo network which will contain things which are to
+        # be simulated on the host.
+        self.host_network = nengo.Network()
+
+    def get_object_for_node(self, node, seed):
+        """Get an intermediate object for the Node."""
+        # TODO Identify when a function of time Node is being passed and act
+        # differently.
+        return IntermediateHostNode(node, seed)
+
+    def get_source_for_connection(self, conn, irn):
+        """Get a source for a connection from a Node."""
+        return self.get_source_for_node(conn.pre_obj)
+
+    def get_source_for_node(self, node):
+        # Get the SpiNNaker source objects
+        spinn_source = self.get_spinnaker_source_for_node(node)
+
+        # Add the Node, a new OutputNode and a connection between them to the
+        # host network.
+        with self.host_network:
+            self.host_network.add(node)
+            o_node = OutputNode(node, self)
+            nengo.Connection(node, o_node, synapse=None)
+
+        # Return the SpiNNaker source objects
+        return spinn_source
+
+    def get_spinnaker_source_for_node(self, node):  # pragma : no cover
+        """Get the source for a connection from a given Node.
+
+        **OVERRIDE THIS METHOD**  The return type should be as documented for
+        `nengo_spinnaker.intermediate_representation.\
+         IntermediateRepresentation.source_getters`.
+
+        Parameters
+        ----------
+        node : :py:class:`nengo.Node`
+        """
+        raise NotImplementedError
+
+    def get_sink_for_node(self, conn, irn):
+        """Get a sink for a connection into a Node."""
+        # Get the SpiNNaker sink objects
+        spinn_sink = self.get_spinnaker_sink_for_node(conn.post_obj)
+
+        # Add the Node, a new InputNode and a connection between them to the
+        # host network.
+        with self.host_network:
+            self.host_network.add(conn.post_obj)
+            i_node = InputNode(conn.post_obj, self)
+            nengo.Connection(i_node, conn.post_obj, synapse=None)
+
+        # Return the SpiNNaker sink objects
+        return spinn_sink
+
+    def get_spinnaker_sink_for_node(self, node):  # pragma : no cover
+        """Get the sink for a connection to a given Node.
+
+        **OVERRIDE THIS METHOD**  The return type should be as documented for
+        `nengo_spinnaker.intermediate_representation.\
+         IntermediateRepresentation.sink_getters`.
+
+        Parameters
+        ----------
+        node : :py:class:`nengo.Node`
+        """
+        raise NotImplementedError
+
+    def get_probe_for_node(self, probe, seed, irn):
+        """Get a probe object for the Node."""
+        # Get a source for the Node; then add a new probe object and a
+        # connection from the source to the probe.
+        source, extras = self.get_source_for_node(probe.target)
+        probe_object = IntermediateObject(probe, seed)
+        probe_conn = IntermediateNet(
+            seed,
+            nl.NetAddress(source, nl.OutputPort.standard),
+            nl.NetAddress(probe_object, nl.InputPort.standard),
+            keyspace=extras.pop("keyspace", None),
+            latching=extras.pop("latching", False)
+        )
+
+        objects = extras.pop("extra_objects", list()) + [source]
+        conns = extras.pop("extra_connections", list()) + [probe_conn]
+
+        return probe_object, objects, conns
+
+    def get_node_input(self, node):  # pragma : no cover
+        """Return the current input value for a Node.
+
+        Parameters
+        ----------
+        node : :py:class:`nengo.Node`
+
+        Returns
+        -------
+        array
+            Last received input value for a Node.
+        """
+        raise NotImplementedError
+
+    def set_node_output(self, node, value):  # pragma : no cover
+        """Send the current output of a Node to SpiNNaker.
+
+        Parameters
+        ----------
+        node : :py:class:`nengo.Node`
+        value : array
+        """
+        raise NotImplementedError
+
+
+class InputNode(nengo.Node):
+    """Node which transmits simulation data to SpiNNaker."""
+    def __init__(self, node, io_controller):
+        self.target = node
+        self.controller = io_controller
+        self.size_in = 0
+        self.size_out = node.size_in
+
+    def output(self, t):
+        return self.controller.get_node_input(self.target)
+
+
+class OutputNode(nengo.Node):
+    """Node which transmits simulation data to SpiNNaker."""
+    def __init__(self, node, io_controller):
+        self.target = node
+        self.controller = io_controller
+        self.size_in = node.size_out
+        self.size_out = 0
+
+    def output(self, t, value):
+        self.controller.set_node_output(self.target, value)
---
+++ nengo_spinnaker/tests/test_node.py
@@ -0,0 +1,194 @@
+import mock
+import nengo
+import pytest
+
+from nengo_spinnaker import intermediate_representation as ir
+from nengo_spinnaker import netlist as nl
+from nengo_spinnaker.node import (NodeIOController, IntermediateHostNode,
+                                  OutputNode, InputNode)
+
+
+class TestInputNode(object):
+    @pytest.mark.parametrize(
+        "node",
+        [nengo.Node(lambda t, x: None, size_in=5, add_to_container=False),
+         nengo.Node(lambda t, x: None, size_in=2, add_to_container=False)]
+    )
+    def test_init(self, node):
+        cn = mock.Mock(spec_set=NodeIOController, name="IO Controller")
+        input_node = InputNode(node, cn, add_to_container=False)
+
+        assert input_node.size_in == 0
+        assert input_node.size_out == node.size_in
+        assert input_node.target is node
+        assert input_node.controller is cn
+
+        # Check that calling the input works
+        cn.get_node_input.return_value = [300]
+        assert input_node.output(0.5) == cn.get_node_input.return_value
+        cn.get_node_input.assert_called_once_with(node)
+
+
+class TestOutputNode(object):
+    @pytest.mark.parametrize(
+        "node",
+        [nengo.Node(lambda t, x: None, size_in=5, add_to_container=False),
+         nengo.Node(lambda t, x: None, size_in=2, add_to_container=False)]
+    )
+    def test_init(self, node):
+        cn = mock.Mock(spec_set=NodeIOController, name="IO Controller")
+        output_node = OutputNode(node, cn, add_to_container=False)
+
+        assert output_node.size_in == node.size_out
+        assert output_node.size_out == 0
+        assert output_node.target is node
+        assert output_node.controller is cn
+
+        # Check that calling the output works
+        output_node.output(0.5, [300])
+        cn.set_node_output.assert_called_once_with(node, [300])
+
+
+class TestNodeIOController(object):
+    """Test for the base NodeIOController implementation."""
+
+    def test_get_object_for_node(self):
+        """Test returning an intermediate object for a Node."""
+        node = mock.Mock(spec_set=[])
+
+        # Should return an IntermediateHostNode
+        nioc = NodeIOController()
+        ihn = nioc.get_object_for_node(node, 1234)
+
+        assert isinstance(ihn, IntermediateHostNode)
+        assert ihn.seed == 1234
+
+    def test_get_source_for_connection(self):
+        """Test that getting the source for a Node calls the subclasses
+        get_spinnaker_source_for_node method correctly, and adds a new Node and
+        connection to the host network.
+        """
+        # Construct the Node and the connection
+        node = nengo.Node(1, add_to_container=False)
+        conn = mock.Mock(spec=[nengo.Connection])
+        conn.pre_obj = node
+
+        # Construct a mock IRN
+        ir_node = IntermediateHostNode(node, 323)
+        irn = ir.IntermediateRepresentation({node: ir_node}, {}, [], [])
+
+        # Construct a mock NodeIOController
+        nioc = NodeIOController()
+        with mock.patch.object(nioc, "get_spinnaker_source_for_node") as fn:
+            fn.return_value = None, {}
+
+            # Make the call to get the SpiNNaker source, assert that this call
+            # is passed on
+            assert nioc.get_source_for_connection(conn, irn) == fn.return_value
+            fn.assert_called_once_with(node)
+
+        # Assert that the host network is now not empty
+        assert len(nioc.host_network.all_connections) == 1
+        assert len(nioc.host_network.all_nodes) == 2
+
+        # We should have the node itself and an output Node which points at it
+        for n in nioc.host_network.all_nodes:
+            if isinstance(n, OutputNode):
+                assert n.target is node
+                assert n.controller is nioc
+            else:
+                assert n is node
+
+        # The connection should link these two items together
+        c = nioc.host_network.all_connections[0]
+        assert c.pre_obj is node
+        assert c.post_obj.target is node
+
+    def test_get_sink_for_node(self):
+        """Test that getting the sink for a Node calls the subclasses
+        get_spinnaker_sink_for_node method correctly, and adds a new Node and
+        connection to the host network.
+        """
+        # Construct the Node and the connection
+        node = nengo.Node(lambda t, v: None, size_in=3, add_to_container=False)
+        conn = mock.Mock(spec=[nengo.Connection])
+        conn.post_obj = node
+
+        # Construct a mock IRN
+        ir_node = IntermediateHostNode(node, 323)
+        irn = ir.IntermediateRepresentation({node: ir_node}, {}, [], [])
+
+        # Construct a mock NodeIOController
+        nioc = NodeIOController()
+        with mock.patch.object(nioc, "get_spinnaker_sink_for_node") as fn:
+            fn.return_value = None, {}
+
+            # Make the call to get the SpiNNaker sink, assert that this call
+            # is passed on
+            assert nioc.get_sink_for_node(conn, irn) == fn.return_value
+            fn.assert_called_once_with(node)
+
+        # Assert that the host network is now not empty
+        assert len(nioc.host_network.all_connections) == 1
+        assert len(nioc.host_network.all_nodes) == 2
+
+        # We should have the node itself and an input Node which points at it
+        for n in nioc.host_network.all_nodes:
+            if isinstance(n, InputNode):
+                assert n.target is node
+                assert n.controller is nioc
+            else:
+                assert n is node
+
+        # The connection should link these two items together
+        c = nioc.host_network.all_connections[0]
+        assert c.pre_obj.target is node
+        assert c.post_obj is node
+
+    def test_get_probe_for_node(self):
+        """Test that getting the probe for a Node gets the source for the Node
+        and adds this and an extra connection to the intermediate
+        representation.
+        """
+        # Build the Node, the Probe and an empty irn
+        with nengo.Network():
+            a = nengo.Node(lambda t: t**2, size_out=1)
+            p = nengo.Probe(a)
+
+        # Construct a mock IRN
+        ir_node = IntermediateHostNode(a, 323)
+        irn = ir.IntermediateRepresentation({a: ir_node}, {}, [], [])
+
+        # Construct some extra objects and extra connections
+        extra_objects = [mock.Mock(name="extra obj")]
+        extra_connections = [mock.Mock(name="extra connection")]
+        keyspace = mock.Mock(name="keyspace")
+        source = mock.Mock(name="source")
+
+        # Construct a mock NodeIOController
+        nioc = NodeIOController()
+        with mock.patch.object(nioc, "get_source_for_node") as fn:
+            fn.return_value = (source,
+                               {"extra_objects": extra_objects,
+                                "extra_connections": extra_connections,
+                                "latching": True,
+                                "keyspace": keyspace})
+
+            ir_probe, extra_objs, extra_conns = nioc.get_probe_for_node(
+                p, 454, irn)
+
+        # Check that all of the returned objects are sensible
+        assert isinstance(ir_probe, ir.IntermediateObject)
+
+        assert extra_objs == extra_objects + [source]
+        assert len(extra_conns) == len(extra_connections) + 1
+
+        for c in (x for x in extra_conns if x not in extra_connections):
+            assert isinstance(c, ir.IntermediateNet)
+            assert c.seed == 454
+            assert c.source.object is source
+            assert c.source.port is nl.OutputPort.standard
+            assert c.sink.object is ir_probe
+            assert c.sink.port is nl.InputPort.standard
+            assert c.keyspace is keyspace
+            assert c.latching
