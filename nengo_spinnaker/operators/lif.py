4a265c2bd17997e2155e7f69921930374a538eea
AndrewMundy <andrew.mundy@ieee.org>
2015-04-29 14:05:22 +0100

    LIF ensemble system region.

--- nengo_spinnaker/operators/lif.py
+++ nengo_spinnaker/operators/lif.py
@@ -1,4 +1,17 @@
+"""LIF Ensemble
+
+Takes an intermediate representation of a LIF ensemble and returns a vertex and
+appropriate callbacks to load and prepare the ensemble for simulation on
+SpiNNaker.  The build method also manages the partitioning of the ensemble into
+appropriate sized slices.
+"""
+
+import collections
 import numpy as np
+from rig import type_casts
+import struct
+
+s1615 = type_casts.float_to_fix(True, 32, 15)
 
 
 class EnsembleLIF(object):
@@ -7,3 +20,33 @@ class EnsembleLIF(object):
         """Create a new LIF ensemble controller."""
         self.direct_input = np.zeros(size_in)
         self.local_probes = list()
+
+
+class SystemRegion(collections.namedtuple(
+    "SystemRegion", "n_input_dimensions, n_output_dimensions, "
+                    "machine_timestep, t_ref, t_rc, dt, probe_spikes")):
+    """Region of memory describing the general parameters of a LIF ensemble."""
+
+    def sizeof(self):
+        """Get the number of bytes necessary to represent this region of
+        memory.
+        """
+        return 8 * 4  # 8 words
+
+    def write_subregion_to_file(self, vertex_slice, fp):
+        """Write the system region for a specific vertex slice to a file-like
+        object.
+        """
+        n_neurons = vertex_slice.stop - vertex_slice.start
+        data = struct.pack(
+            "<8I",
+            self.n_input_dimensions,
+            self.n_output_dimensions,
+            n_neurons,
+            self.machine_timestep,
+            int(self.t_ref // self.dt),
+            s1615(self.dt / self.t_rc),
+            (0x1 if self.probe_spikes else 0x0),
+            1
+        )
+        fp.write(data)
--- tests/operators/test_lif.py
+++ tests/operators/test_lif.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pytest
+import struct
+import tempfile
 
-from nengo_spinnaker.operators import EnsembleLIF
+from rig import type_casts
+s1615 = type_casts.float_to_fix(True, 32, 15)
+
+from nengo_spinnaker.operators import lif
 
 
 class TestEnsembleLIF(object):
@@ -10,6 +15,59 @@ class TestEnsembleLIF(object):
         """Test that creating an Ensemble LIF creates an empty list of local
         probes and an empty input vector.
         """
-        lif = EnsembleLIF(size_in)
-        assert np.all(lif.direct_input == np.zeros(size_in))
-        assert lif.local_probes == list()
+        op = lif.EnsembleLIF(size_in)
+        assert np.all(op.direct_input == np.zeros(size_in))
+        assert op.local_probes == list()
+
+
+class TestSystemRegion(object):
+    """Test system regions for Ensembles."""
+    def test_sizeof(self):
+        region = lif.SystemRegion(1, 5, 1000, 0.01, 0.02, 0.001, False)
+        assert region.sizeof() == 8 * 4  # 8 words
+
+    @pytest.mark.parametrize(
+        "vertex_slice, vertex_neurons",
+        [(slice(0, 100), 100),
+         (slice(100, 120), 20),
+         ]
+    )
+    @pytest.mark.parametrize(
+        "machine_timestep, dt, size_in, tau_ref, tau_rc, "
+        "size_out, probe_spikes",
+        [(1000, 0.001, 5, 0.0, 0.002, 7, True),
+         (10000, 0.01, 1, 0.001, 0.02, 3, False),
+         ]
+    )
+    def test_write_subregion_to_file(self, machine_timestep, dt,
+                                     size_in, tau_ref, tau_rc,
+                                     size_out, probe_spikes,
+                                     vertex_slice, vertex_neurons):
+        # Check that the region is correctly written to file
+        region = lif.SystemRegion(
+            size_in, size_out, machine_timestep, tau_ref, tau_rc,
+            dt, probe_spikes
+        )
+
+        # Create the file
+        fp = tempfile.TemporaryFile()
+
+        # Write to it
+        region.write_subregion_to_file(vertex_slice, fp)
+
+        # Read back and check that the values are sane
+        fp.seek(0)
+        values = fp.read()
+        assert len(values) == region.sizeof()
+
+        (n_in, n_out, n_n, m_t, t_ref, dt_over_t_rc, rec_spikes, i_dims) = \
+            struct.unpack_from("<8I", values)
+        assert n_in == size_in
+        assert n_out == size_out
+        assert n_n == vertex_neurons
+        assert m_t == machine_timestep
+        assert t_ref == int(tau_ref // dt)
+        assert dt_over_t_rc == s1615(dt / tau_rc)
+        assert ((probe_spikes and rec_spikes != 0) or
+                (not probe_spikes and rec_spikes == 0))
+        assert i_dims == 1
