6f9d99f74d2dd31ccad0ea373b7a2209d4337c2c
AndrewMundy <andrew.mundy@ieee.org>
2015-05-02 12:06:31 +0100

    Add homogeneous partitioner.

--- nengo_spinnaker/partition_and_cluster.py
+++ nengo_spinnaker/partition_and_cluster.py
@@ -2,9 +2,135 @@
 resulting subvertices.
 """
 import collections
+import math
 from six import iteritems, itervalues
 
 from .keyspaces import is_nengo_keyspace
+
+
+class Constraint(collections.namedtuple("Constraint",
+                                        "maximum, target, max_usage")):
+    """Constraint on a resource.
+
+    Attributes
+    ----------
+    maximum : number
+        Hard constraint on maximum usage of the resource.
+    target : float
+        Explicit cap on the amount of the maximum that may be used.
+    max_usage : float
+        `maximum * target`
+
+    For example, to define a target of 90% 64KiB-DTCM usage::
+
+        dtcm_constraint = Constraint(64 * 2**10, 0.9)
+    """
+    def __new__(cls, maximum, target=1.0):
+        """Create a new constraint.
+
+        Parameters
+        ----------
+        maximum : number
+            Hard constraint on maximum usage of the resource.
+        target : float (optional, default=1.0)
+            Explicit cap on the amount of the maximum that may be used.
+        """
+        return super(Constraint, cls).__new__(
+            cls, maximum, target, maximum*target
+        )
+
+
+def partition(initial_slice, constraints_and_getters):
+    """Construct a list of slices which satisfy a set of constraints.
+
+    Parameters
+    ----------
+    initial_slice : :py:class:`slice`
+        Initial partition of the object, this should represent everything.
+    constraints_and_getters : {:py:class:`~.Constraint`: func, ...}
+        Dictionary mapping constraints to functions which will accept a slice
+        and return the current usage of the resource for the given slice.
+
+    ..note::
+        It is assumed that the object being sliced is homogeneous, i.e., there
+        is no difference in usage for `slice(0, 10)` and `slice(10, 20)`.
+
+    Yields
+    ------
+    :py:class:`slice`
+        Slices which meet satisfy all the constraints.
+
+    Raises
+    ------
+    UnpartitionableError
+        If the given problem cannot be solved by this partitioner.
+    """
+    def constraints_unsatisfied(slices, constraints):
+        for s in slices:
+            for constraint, usage in iteritems(constraints):
+                yield constraint.max_usage < usage(s)
+
+    # Normalise the slice
+    if initial_slice.start is None:
+        initial_slice = slice(0, initial_slice.stop)
+
+    # While any of the slices fail to satisfy a constraint we partition further
+    n_cuts = 1
+    max_cuts = initial_slice.stop - initial_slice.start
+    slices = [initial_slice]
+
+    while any(constraints_unsatisfied(slices, constraints_and_getters)):
+        if n_cuts == 1:
+            # If we haven't performed any partitioning then we get the first
+            # number of cuts to make.
+            n_cuts = max(
+                int(math.ceil(usage(initial_slice) / c.max_usage)) for
+                c, usage in iteritems(constraints_and_getters)
+            )
+        else:
+            # Otherwise just increment the number of cuts rather than honing in
+            # on the expensive elements.
+            n_cuts += 1
+
+        if n_cuts > max_cuts:
+            # We can't cut any further, so the problem can't be solved.
+            raise UnpartitionableError
+
+        # Partition
+        slices = divide_slice(initial_slice, n_cuts)
+
+    # Yield the partitioned slices
+    for s in divide_slice(initial_slice, n_cuts):
+        yield s
+
+
+def divide_slice(initial_slice, n_slices):
+    """Create a set of smaller slices from an original slice.
+
+    Parameters
+    ----------
+    initial_slice : :py:class:`slice`
+        A slice which must have `start` and `stop` set.
+    n_slices : int
+        Number of slices to produce.
+
+    Yields
+    ------
+    :py:class:`slice`
+        Slices which when combined would be equivalent to `initial_slice`.
+    """
+    start = initial_slice.start
+    stop = initial_slice.stop
+    chunk = int(math.ceil((stop - start) / n_slices))
+    pos = start
+
+    while pos < stop:
+        yield slice(pos, min(pos + chunk, stop))
+        pos += chunk
+
+
+class UnpartitionableError(Exception):
+    """Indicates that a given partitioning problem cannot be solved."""
 
 
 def identify_clusters(placed_vertices, nets, groups):
--- tests/test_partition_and_cluster.py
+++ tests/test_partition_and_cluster.py
@@ -6,6 +6,70 @@ from nengo_spinnaker import partition_and_cluster as pac
 from nengo_spinnaker.keyspaces import KeyspaceContainer
 
 
+def test_constraint():
+    """Test creating a constraint."""
+    # Constraints consist of: a limit, and an optional target.
+    constraint = pac.Constraint(100)
+    assert constraint.maximum == 100
+    assert constraint.target == 1.0
+    assert constraint.max_usage == 100.0
+
+    constraint = pac.Constraint(100, 0.9)
+    assert constraint.maximum == 100
+    assert constraint.target == 0.9
+    assert constraint.max_usage == 90.0
+
+
+class TestPartition(object):
+    """Test partitioning slices."""
+    def test_no_partitioning(self):
+        # Create the constraint
+        constraint = pac.Constraint(100, 0.9)
+
+        # Create the constraint -> usage mapping
+        constraints = {constraint: lambda sl: sl.stop - sl.start + 10}
+
+        # Perform the partitioning
+        assert list(pac.partition(slice(0, 80), constraints)) == [slice(0, 80)]
+        assert list(pac.partition(slice(80), constraints)) == [slice(0, 80)]
+
+    def test_single_partition_step(self):
+        # Create the constraint
+        constraint_a = pac.Constraint(100, .7)
+        constraint_b = pac.Constraint(50)
+
+        # Create the constraint -> usage mapping
+        constraints = {constraint_a: lambda sl: sl.stop - sl.start + 10,
+                       constraint_b: lambda sl: sl.stop - sl.start}
+
+        # Perform the partitioning
+        assert list(pac.partition(slice(100), constraints)) == [
+            slice(0, 50), slice(50, 100)
+        ]
+
+    def test_just_partitionable(self):
+        # Create the constraint
+        constraint_a = pac.Constraint(50)
+
+        # Create the constraint -> usage mapping
+        constraints = {constraint_a: lambda sl: sl.stop - sl.start + 49}
+
+        # Perform the partitioning
+        assert list(pac.partition(slice(100), constraints)) == \
+            [slice(n, n+1) for n in range(100)]
+
+    def test_unpartitionable(self):
+        # Create the constraint
+        constraint_a = pac.Constraint(50)
+
+        # Create the constraint -> usage mapping
+        constraints = {constraint_a: lambda sl: sl.stop - sl.start + 100}
+
+        # Perform the partitioning
+        with pytest.raises(pac.UnpartitionableError):
+            list(pac.partition(slice(100), constraints))
+
+
 def test_identify_clusters():
     """Test the correct assignation of clusters, modification of vertex slices
     and net keyspaces.
