0f2f60ca8da7ecdc27943f644130d3682b480976
AndrewMundy <andrew.mundy@ieee.org>
2015-05-04 08:39:48 +0100

    Netlists place and load themselves.

--- nengo_spinnaker/netlist.py
+++ nengo_spinnaker/netlist.py
@@ -1,6 +1,13 @@
 """Higher and lower level netlist items.
 """
 import rig.netlist
+from rig.place_and_route import place, allocate, route
+from rig.place_and_route.utils import (build_application_map,
+                                       build_routing_tables)
+from rig.machine_control.utils import sdram_alloc_for_vertices
+
+from .partition_and_cluster import identify_clusters
+from .utils.itertools import flatten
 
 
 class Net(rig.netlist.Net):
@@ -101,3 +108,120 @@ class VertexSlice(Vertex):
                  constraints=list()):
         super(VertexSlice, self).__init__(application, resources, constraints)
         self.slice = slice
+
+
+class Netlist(object):
+    """A netlist represents a set of executables to run on a SpiNNaker machine
+    and their communication links.
+
+    Attributes
+    ----------
+    nets : [:py:class:`~.Net`, ...]
+        List of nets (communication).
+    vertices : [:py:class:`~.Vertex` or :py:class:`~.VertexSlice`, ...]
+        List of vertex objects (executables).
+    keyspaces : :py:class:`~nengo_spinnaker.utils.keyspaces.KeyspaceContainer`
+        Object containing keyspaces for nets.
+    groups : {:py:class:`~.Vertex`: int, ...}
+        Map of vertices to their group numbers.
+    load_functions : [`fn(netlist, controller)`, ...]
+        List of functions which will be called to load the model to a SpiNNaker
+        machine.  Each must accept a netlist and a controller.
+    before_simulation_functions : [`fn(netlist, controller, n_steps)`, ...]
+        List of functions which will be called to prepare the executables for a
+        number of simulation steps.  Each must accept a netlist, a controller
+        and a number of simulation steps.
+    after_simulation_functions : [`fn(netlist, controller, n_steps)`, ...]
+        List of functions which will be called to clean the executables after a
+        number of simulation steps.  Each must accept a netlist, a controller
+        and a number of simulation steps.
+    placements : {vertex: (x, y), ...}
+        Map from vertices to the chips on which they are placed.
+    allocations : {vertex: {resource: allocation, ...}, ...}
+        Map of vertices to the resources they have been assigned.
+    routes : {net: routing tree, ...}
+        Map of nets to the routes through the machine to which they correspond.
+    vertices_memory : {vertex: filelike, ...}
+        Map of vertices to file-like views of the SDRAM they have been
+        allocated.
+    """
+    def __init__(self, nets, vertices, keyspaces, groups,
+                 load_functions=list(), before_simulation_functions=list(),
+                 after_simulation_functions=list()):
+        # Store given parameters
+        self.nets = list(nets)
+        self.vertices = list(vertices)
+        self.keyspaces = keyspaces
+        self.groups = dict(groups)
+        self.load_functions = list(load_functions)
+        self.before_simulation_functions = list(before_simulation_functions)
+        self.after_simulation_functions = list(after_simulation_functions)
+
+        # Create containers for the attributes that are filled in by place and
+        # route.
+        self.placements = dict()
+        self.allocations = dict()
+        self.routes = dict()
+        self.vertices_memory = dict()
+
+    def place_and_route(self, machine):
+        """Place and route the netlist onto the given SpiNNaker machine.
+
+        Parameters
+        ----------
+        machine : :py:class:`~rig.machine.Machine`
+            Machine onto which the netlist should be placed and routed.
+        """
+        # Build a map of vertices to the resources they require, get a list of
+        # constraints.
+        vertices_resources = {v: v.resources for v in self.vertices}
+        constraints = list(flatten(v.constraints for v in self.vertices))
+
+        # Perform placement and allocation
+        self.placements = place(vertices_resources, self.nets,
+                                machine, constraints)
+        self.allocations = allocate(vertices_resources, self.nets, machine,
+                                    constraints, self.placements)
+
+        # Identify clusters and modify keyspaces and vertices appropriately
+        identify_clusters(self.placements, self.nets, self.groups)
+
+        # Fix all keyspaces
+        self.keyspaces.assign_fields()
+
+        # Finally, route all nets
+        self.routes = route(vertices_resources, self.nets, machine,
+                            constraints, self.placements, self.allocations)
+
+    def load_application(self, controller):
+        """Load the netlist to a SpiNNaker machine.
+
+        Parameters
+        ----------
+        controller : :py:class:`~rig.machine_control.MachineController`
+            Controller to use to communicate with the machine.
+        """
+        # Build and load the routing tables, first by building a mapping from
+        # nets to keys and masks.
+        net_keys = {n: (n.keyspace.get_value(tag=self.keyspaces.routing_tag),
+                        n.keyspace.get_mask(tag=self.keyspaces.routing_tag))
+                    for n in self.nets}
+        routing_tables = build_routing_tables(self.routes, net_keys)
+        controller.load_routing_tables(routing_tables)
+
+        # Assign memory to each vertex as required
+        self.vertices_memory = sdram_alloc_for_vertices(
+            controller, self.placements, self.allocations
+        )
+
+        # Call each loading function in turn
+        for fn in self.load_functions:
+            fn(self, controller)
+
+        # Load the applications onto the machine
+        vertices_applications = {v: v.application for v in self.vertices
+                                 if v.application is not None}
+        application_map = build_application_map(
+            vertices_applications, self.placements, self.allocations
+        )
+        controller.load_application(application_map)
--- tests/test_netlist.py
+++ tests/test_netlist.py
@@ -2,8 +2,10 @@ import mock
 import pytest
 from rig.bitfield import BitField
 from rig import machine
+from rig.machine import Cores, SDRAM
 
 from nengo_spinnaker import netlist
+from nengo_spinnaker.utils.itertools import flatten
 
 
 def test_vertex():
@@ -93,3 +95,299 @@ class TestNet(object):
         assert "keyspace" in err_string
         assert "32" in err_string
         assert "{}".format(length) in err_string
+
+
+def test_place_and_route():
+    """Test that Netlists can place and route themselves.
+
+    Provide two vertices and net and ensure that the correct methods are called
+    to place and allocate, fix the bitfield and then route the model.  Ensure
+    that the returned values are stored.
+    """
+    # Create the objects to store in the Netlist
+    v1 = netlist.Vertex("test_app", resources={Cores: 1, SDRAM: 400},
+                        constraints=[mock.Mock(name="constraint1")])
+    v2a = netlist.Vertex("test_app", resources={Cores: 2, SDRAM: 100},
+                         constraints=[mock.Mock(name="constraint2"),
+                                      mock.Mock(name="constraint3")])
+    v2b = netlist.Vertex("test_app", resources={Cores: 2, SDRAM: 100},
+                         constraints=[mock.Mock(name="constraint2"),
+                                      mock.Mock(name="constraint3")])
+
+    keyspace = mock.Mock(name="keyspace")
+    keyspace.length = 32
+    net = netlist.Net(v1, [v2a, v2b], 1, keyspace)
+    groups = {v2a: 1, v2b: 1}
+
+    keyspace_container = mock.Mock()
+
+    # Create the Netlist
+    nl = netlist.Netlist(
+        nets=[net],
+        vertices=[v1, v2a, v2b],
+        keyspaces=keyspace_container,
+        groups=groups,
+        load_functions=list(),
+        before_simulation_functions=list(),
+        after_simulation_functions=list()
+    )
+
+    # Patch out all the methods that should be called
+    with \
+            mock.patch("nengo_spinnaker.netlist.place") as place, \
+            mock.patch("nengo_spinnaker.netlist.allocate") as allocate, \
+            mock.patch(
+                "nengo_spinnaker.netlist.identify_clusters") as cluster, \
+            mock.patch("nengo_spinnaker.netlist.route") as route:
+        # Create a mock machine to place/route against
+        machine = mock.Mock(name="machine")
+
+        # Set up these methods to ensure that the call order is correct and
+        # that they return appropriate objects
+        def place_fn(resources, nets, machine, constraints):
+            # Check that the arguments were correct
+            assert resources == {v: v.resources for v in nl.vertices}
+            assert nets == nl.nets
+            assert constraints == list(flatten(  # pragma : no branch
+                v.constraints for v in nl.vertices))
+
+            # Return some placements
+            return {v1: (0, 0), v2a: (0, 1), v2b: (1, 0)}
+
+        place.side_effect = place_fn
+
+        def allocate_fn(resources, nets, machine, constraints, placements):
+            assert place.call_count == 1
+
+            # Check that the arguments were correct
+            assert resources == {v: v.resources for v in nl.vertices}
+            assert nets == nl.nets
+            assert constraints == list(flatten(  # pragma : no branch
+                v.constraints for v in nl.vertices))
+            assert placements == {v1: (0, 0), v2a: (0, 1), v2b: (1, 0)}
+
+            # Return some allocations
+            return {
+                v1: {Cores: slice(0, 1)},
+                v2a: {Cores: slice(9, 10)},
+                v2b: {Cores: slice(4, 6)},
+            }
+
+        allocate.side_effect = allocate_fn
+
+        def cluster_fn(placed_vertices, nets, groups):
+            assert allocate.call_count == 1
+
+            # Check that the arguments are correct
+            assert placed_vertices == {v1: (0, 0), v2a: (0, 1), v2b: (1, 0)}
+            assert nets == nl.nets
+            assert groups == nl.groups
+
+        cluster.side_effect = cluster_fn
+
+        def assign_fields_fn():
+            assert cluster.call_count == 1
+
+        keyspace_container.assign_fields.side_effect = assign_fields_fn
+
+        routing_tree = mock.Mock(name="routing tree")
+
+        def route_fn(resources, nets, machine_, constraints, placements,
+                     allocations):
+            assert keyspace_container.assign_fields.call_count == 1
+
+            # Check that the arguments are correct
+            assert resources == {v: v.resources for v in nl.vertices}
+            assert nets == nl.nets
+            assert machine_ is machine
+            assert constraints == list(flatten(  # pragma : no branch
+                v.constraints for v in nl.vertices))
+            assert placements == {v1: (0, 0), v2a: (0, 1), v2b: (1, 0)}
+            assert allocations == {v1: {Cores: slice(0, 1)},
+                                   v2a: {Cores: slice(9, 10)},
+                                   v2b: {Cores: slice(4, 6)}, }
+
+            # Return some routes
+            return {net: routing_tree}
+
+        route.side_effect = route_fn
+
+        # Assert starting from a blank slate
+        assert nl.placements == dict()
+        assert nl.allocations == dict()
+        assert nl.routes == dict()
+
+        # Perform the place and route for the machine
+        nl.place_and_route(machine)
+
+        # Assert methods were called once each
+        assert place.call_count == 1
+        assert allocate.call_count == 1
+        assert cluster.call_count == 1
+        assert keyspace_container.assign_fields.call_count == 1
+        assert route.call_count == 1
+
+        # Assert that the placements, etc. were recorded
+        assert nl.placements == {v1: (0, 0), v2a: (0, 1), v2b: (1, 0)}
+        assert nl.allocations == {v1: {Cores: slice(0, 1)},
+                                  v2a: {Cores: slice(9, 10)},
+                                  v2b: {Cores: slice(4, 6)}, }
+        assert nl.routes == {net: routing_tree}
+
+
+def test_load_application():
+    """Test the steps involved in loading an application to a SpiNNaker
+    machine.
+
+     - Building and loading routing tables
+     - Allocating SDRAM for vertices which require it
+     - Calling application loading functions
+     - Loading applications to the machine
+    """
+    # Mock controller which will be used to load the model
+    controller = mock.Mock(name="controller")
+
+    # Create the objects to store in the model
+    v1 = netlist.Vertex("test_app", resources={Cores: 1, SDRAM: 400},
+                        constraints=[mock.Mock(name="constraint1")])
+    v2a = netlist.Vertex("test_app2", resources={Cores: 2, SDRAM: 100},
+                         constraints=[mock.Mock(name="constraint2"),
+                                      mock.Mock(name="constraint3")])
+    v2b = netlist.Vertex("test_app2", resources={Cores: 2, SDRAM: 100},
+                         constraints=[mock.Mock(name="constraint2"),
+                                      mock.Mock(name="constraint3")])
+
+    keyspace_container = mock.Mock(name="keyspace container")
+    keyspace_container.routing_tag = "bob"
+
+    keyspace = mock.Mock(name="keyspace")
+    keyspace.length = 32
+
+    def get_mask(tag):
+        assert tag == keyspace_container.routing_tag
+        return 0xffff0000
+
+    def get_value(tag):
+        assert tag == keyspace_container.routing_tag
+        return 0x33330000
+
+    keyspace.get_mask.side_effect = get_mask
+    keyspace.get_value.side_effect = get_value
+
+    net = netlist.Net(v1, [v2a, v2b], 1, keyspace)
+
+    groups = {v2a: 1, v2b: 1}
+
+    routing_tree = mock.Mock(name="routing tree")
+
+    # Create the model
+    model = netlist.Netlist(
+        nets=[net],
+        vertices=[v1, v2a, v2b],
+        keyspaces=keyspace_container,
+        groups=groups,
+        load_functions=list(),
+        before_simulation_functions=list(),
+        after_simulation_functions=list()
+    )
+    model.placements = {v1: (0, 0), v2a: (0, 1), v2b: (1, 0)}
+    model.allocations = {
+        v1: {Cores: slice(0, 1), SDRAM: slice(0, 400)},
+        v2a: {Cores: slice(9, 11), SDRAM: slice(300, 400)},
+        v2b: {Cores: slice(9, 11), SDRAM: slice(300, 400)},
+    }
+    model.routes = {net: routing_tree}
+
+    # Mock routing table
+    routing_table = mock.Mock(name="routing table")
+
+    # Patch out all the methods that should be called
+    with \
+            mock.patch("nengo_spinnaker.netlist.build_routing_tables") as \
+            build_routing_tables, \
+            mock.patch("nengo_spinnaker.netlist.build_application_map") as \
+            build_application_map, \
+            mock.patch("nengo_spinnaker.netlist.sdram_alloc_for_vertices") as \
+            sdram_alloc:
+
+        # Create replacement methods for patched methods
+        def build_routing_tables_fn(routes, net_keys, **kwargs):
+            # Assert that the arguments were correct
+            assert routes == model.routes
+            assert net_keys == {net: (0x33330000, 0xffff0000)}
+
+            # Assert that the keyspace was only called once
+            assert keyspace.get_mask.call_count == 1
+            assert keyspace.get_value.call_count == 1
+
+            # Return a routing table
+            return routing_table
+
+        build_routing_tables.side_effect = build_routing_tables_fn
+
+        def build_application_map_fn(vertices_applications, placements,
+                                     allocations):
+            # Assert that the arguments are correct
+            assert vertices_applications == {v: v.application for v in
+                                             model.vertices}
+            assert placements == model.placements
+            assert allocations == model.allocations
+
+            return {
+                "test_app": {(0, 0): set([0])},
+                "test_app2": {(0, 1): set([9, 10]),
+                              (1, 0): set([9, 10]),
+                              }
+            }
+
+        build_application_map.side_effect = build_application_map_fn
+
+        sdram_allocs = {
+            v1: mock.Mock(),
+            v2a: mock.Mock(),
+            v2b: mock.Mock(),
+        }
+
+        def sdram_alloc_fn(cn, placements, allocations):
+            assert cn is controller
+            assert placements == model.placements
+            assert allocations == model.allocations
+
+            return sdram_allocs
+
+        sdram_alloc.side_effect = sdram_alloc_fn
+
+        # Create some load functions, add them to the model
+        def load_fn(model_, controller_):
+            # Assert that memory is alloced before calling load functions
+            assert sdram_alloc.call_count == 1
+            assert model_.vertices_memory == sdram_allocs
+
+            # Assert that the arguments are correct
+            assert model_ is model
+            assert controller_ is controller
+
+        load_a = mock.Mock(wraps=load_fn)
+        load_b = mock.Mock(wraps=load_fn)
+        model.load_functions = [load_a, load_b]
+
+        # Perform the loading
+        model.load_application(controller)
+
+        # Assert methods were called
+        assert build_routing_tables.call_count == 1
+        assert build_application_map.call_count == 1
+        assert sdram_alloc.call_count == 1
+
+        assert load_a.call_count == 1
+        assert load_b.call_count == 1
+
+        controller.load_routing_tables.assert_called_once_with(routing_table)
+        controller.load_application.assert_called_once_with({
+            "test_app": {(0, 0): set([0])},
+            "test_app2": {(0, 1): set([9, 10]),
+                          (1, 0): set([9, 10]),
+                          }
+        })
+
+        assert model.vertices_memory == sdram_allocs
