d6c4805b00b1cafd62df869541a2f9d6dcb2d1e7
AndrewMundy <andrew.mundy@ieee.org>
2015-05-06 19:01:58 +0100

    Add default source/sink getters

--- nengo_spinnaker/builder/connection.py
+++ nengo_spinnaker/builder/connection.py
@@ -1,7 +1,21 @@
 import nengo
 from nengo.utils.builder import full_transform
 
-from .builder import BuiltConnection, Model
+from .builder import (
+    BuiltConnection, InputPort, Model, ObjectPort, OutputPort, spec
+)
+
+
+@Model.source_getters.register(nengo.base.NengoObject)
+def generic_source_getter(model, conn):
+    obj = model.object_intermediates[conn.pre_obj]
+    return spec(ObjectPort(obj, OutputPort.standard))
+
+
+@Model.sink_getters.register(nengo.base.NengoObject)
+def generic_sink_getter(model, conn):
+    obj = model.object_intermediates[conn.post_obj]
+    return spec(ObjectPort(obj, InputPort.standard))
 
 
 @Model.connection_parameter_builders.register(nengo.base.NengoObject)
--- tests/builder/test_connection.py
+++ tests/builder/test_connection.py
@@ -1,7 +1,59 @@
+import mock
 import nengo
 import numpy as np
 
-from nengo_spinnaker.builder.connection import build_generic_connection_params
+from nengo_spinnaker.builder.builder import InputPort, Model, OutputPort
+from nengo_spinnaker.builder.connection import (
+    generic_source_getter,
+    generic_sink_getter,
+    build_generic_connection_params
+)
+
+
+def test_generic_source_getter():
+    """Test the generic source object getter, this should just return the
+    object that is associated with the connection pre_obj.
+    """
+    # Create the connection
+    conn = mock.Mock(name="connection", spec_set=["pre_obj", "post_obj"])
+    conn.pre_obj = mock.Mock(name="pre object")
+    conn.post_obj = mock.Mock(name="post object")
+
+    # Create the model
+    pre_int = mock.Mock(name="pre intermediate")
+    post_int = mock.Mock(name="post intermediate")
+
+    model = Model()
+    model.object_intermediates[conn.pre_obj] = pre_int
+    model.object_intermediates[conn.post_obj] = post_int
+
+    # Get the source
+    spec = generic_source_getter(model, conn)
+    assert spec.target.obj is pre_int
+    assert spec.target.port is OutputPort.standard
+
+
+def test_generic_sink_getter():
+    """Test the generic sink object getter, this should just return the
+    object that is associated with the connection pre_obj.
+    """
+    # Create the connection
+    conn = mock.Mock(name="connection", spec_set=["pre_obj", "post_obj"])
+    conn.pre_obj = mock.Mock(name="pre object")
+    conn.post_obj = mock.Mock(name="post object")
+
+    # Create the model
+    pre_int = mock.Mock(name="pre intermediate")
+    post_int = mock.Mock(name="post intermediate")
+
+    model = Model()
+    model.object_intermediates[conn.pre_obj] = pre_int
+    model.object_intermediates[conn.post_obj] = post_int
+
+    # Get the sink
+    spec = generic_sink_getter(model, conn)
+    assert spec.target.obj is post_int
+    assert spec.target.port is InputPort.standard
 
 
 def test_build_standard_connection_params():
