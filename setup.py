4ea4c95163205defa22c9af83d7ae78cd4cc2c37
AndrewMundy <andrew.mundy@ieee.org>
2015-05-06 14:25:31 +0100

    Minor fix for setup.py

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(
     license="GPLv2",
 
     # Requirements
-    install_requires=["nengo>=2.0.0", "rig>=0.1.3, <1.0.0"],
+    install_requires=["nengo>=2.0.0, <3.0.0", "rig>=0.1.3, <1.0.0"],
 )
