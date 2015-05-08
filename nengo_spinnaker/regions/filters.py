216d95b645442d58001f361d4fdd1c93d3475cc1
AndrewMundy <andrew.mundy@ieee.org>
2015-05-08 10:17:34 +0100

    Make filter regions.

---
+++ nengo_spinnaker/regions/filters.py
@@ -0,0 +1,202 @@
+import math
+import nengo.synapses
+from rig.type_casts import float_to_fix
+from six import iteritems, itervalues
+import struct
+
+from ..utils.collections import registerabledict
+
+s1615 = float_to_fix(True, 32, 15)
+
+
+def make_filter_regions(signals_and_connections, dt, minimise=False,
+                        filter_routing_tag="filter_routing",
+                        index_field="index_field"):
+    """Create a filter region and a filter routing region from the given
+    signals and connections.
+
+    Parameters
+    ----------
+    signals_and_connections : {Signal: [Connection, ...], ...}
+        Map of signals to the connections they represent.
+    dt : float
+        Simulation timestep.
+
+    Other Parameters
+    ----------------
+    minimise : bool
+        It is possible to reduce the amount of memory and computation required
+        to simulate filters by combining equivalent filters together.  If
+        minimise is `True` then this is done, otherwise not.
+    """
+    # Build the set of filters and the routing entries
+    filters = list()
+    keyspace_routes = list()
+
+    for signal, connections in iteritems(signals_and_connections):
+        for connection in connections:
+            # Make the filter
+            f = FilterRegion.supported_filter_types[type(connection.synapse)].\
+                from_signal_and_connection(signal, connection)
+
+            # Store the filter and add the route
+            for index, g in enumerate(filters):
+                if f == g and minimise:
+                    break
+            else:
+                index = len(filters)
+                filters.append(f)
+
+            keyspace_routes.append((signal.keyspace, index))
+
+    # Create the regions
+    filter_region = FilterRegion(filters, dt)
+    routing_region = FilterRoutingRegion(keyspace_routes, filter_routing_tag,
+                                         index_field)
+    return filter_region, routing_region
+
+
+class FilterRegion(object):
+    """Region of memory which contains filter parameters."""
+
+    supported_filter_types = registerabledict()
+    """Dictionary mapping synapse type to a supported type of filter."""
+
+    def __init__(self, filters, dt):
+        """Create a new filter region."""
+        self.filters = filters
+        self.dt = dt
+
+        # Get the size of the largest supported filter
+        self._largest_struct = max(
+            f.size for f in itervalues(self.supported_filter_types))
+
+    def sizeof(self, *args):
+        """Get the size of the filter region in bytes."""
+        # 1 word + the size of the largest supported filter * number of filters
+        return 4 + self._largest_struct * len(self.filters)
+
+    def write_to_file(self, fp):
+        """Write the region to a file-like object."""
+        data = bytearray(self.sizeof())
+
+        # Write the number of elements
+        struct.pack_into("<I", data, 0, len(self.filters))
+
+        # Pack each element in, allowing enough space for the largest element
+        # in the union.
+        for i, f in enumerate(self.filters):
+            f.pack_into(self.dt, data, 4 + i*self._largest_struct)
+
+        # Write the buffer back
+        fp.write(data)
+
+
+class Filter(object):
+    _pack_chars = "<2I"
+    size = struct.calcsize(_pack_chars)
+
+    def __init__(self, width, latching):
+        self.width = width
+        self.latching = latching
+
+    def __eq__(self, other):
+        return (type(self) is type(other) and
+                self.width == other.width and
+                self.latching == other.latching)
+
+    def pack_into(self, dt, buffer, offset=0):
+        """Pack the struct describing the filter into the buffer."""
+        struct.pack_into(self._pack_chars, buffer, offset,
+                         0xffffffff if self.latching else 0x00000000,
+                         self.width)
+
+
+@FilterRegion.supported_filter_types.register(nengo.synapses.Lowpass)
+class LowpassFilter(Filter):
+    """Represents a Lowpass filter."""
+    _pack_chars = "<2I"
+    size = struct.calcsize(_pack_chars) + struct.calcsize(Filter._pack_chars)
+
+    def __init__(self, width, latching, time_constant):
+        """Create a new Lowpass filter."""
+        super(LowpassFilter, self).__init__(width, latching)
+        self.time_constant = time_constant
+
+    @classmethod
+    def from_signal_and_connection(cls, signal, connection):
+        return cls(
+            connection.size_out, signal.latching, connection.synapse.tau
+        )
+
+    def __eq__(self, other):
+        return (super(LowpassFilter, self).__eq__(other) and
+                self.time_constant == other.time_constant)
+
+    def pack_into(self, dt, buffer, offset=0):
+        """Pack the struct describing the filter into the buffer."""
+        val = math.exp(-dt / self.time_constant)
+        struct.pack_into(self._pack_chars, buffer, offset,
+                         s1615(val),
+                         s1615(1 - val))
+        super(LowpassFilter, self).pack_into(
+            dt, buffer, offset + struct.calcsize(self._pack_chars))
+
+
+@FilterRegion.supported_filter_types.register(type(None))
+class NoneFilter(Filter):
+    """Represents a filter which does nothing."""
+    _pack_chars = "<2I"
+    size = struct.calcsize(_pack_chars) + struct.calcsize(Filter._pack_chars)
+
+    @classmethod
+    def from_signal_and_connection(cls, signal, connection):
+        return cls(connection.size_out, signal.latching)
+
+    def pack_into(self, dt, buffer, offset=0):
+        """Pack the struct describing the filter into the buffer."""
+        struct.pack_into(self._pack_chars, buffer, offset,
+                         s1615(0.0), s1615(1.0))
+        super(NoneFilter, self).pack_into(
+            dt, buffer, offset + struct.calcsize(self._pack_chars))
+
+
+class FilterRoutingRegion(object):
+    """Region of memory which maps routing entries to filter indices.
+
+    Attributes
+    ----------
+    keyspace_routes : [(BitField, int), ...]
+        Pairs of BitFields (keyspaces) to the index of the filter that packets
+        matching the entry should be routed.
+    """
+
+    def __init__(self, keyspace_routes, filter_routing_tag="filter_routing",
+                 index_field="index"):
+        """Create a new routing region."""
+        self.keyspace_routes = keyspace_routes
+        self.filter_routing_tag = filter_routing_tag
+        self.index_field = index_field
+
+    def sizeof(self, *args):
+        """Get the memory requirements of this region as a number of bytes."""
+        # 1 word + 4 words per entry
+        return 4 * (1 + 4*len(self.keyspace_routes))
+
+    def write_to_file(self, fp):
+        """Write the routing region to a file-like object."""
+        data = bytearray(self.sizeof())
+
+        # Write the number of entries
+        struct.pack_into("<I", data, 0, len(self.keyspace_routes))
+
+        # Write each entry in turn
+        for i, (ks, index) in enumerate(self.keyspace_routes):
+            struct.pack_into("<4I", data, 4 + 16*i,
+                             ks.get_value(tag=self.filter_routing_tag),
+                             ks.get_mask(tag=self.filter_routing_tag),
+                             index,
+                             ks.get_mask(field=self.index_field))
+
+        # Write to file
+        fp.write(data)
---
+++ tests/regions/test_filters.py
@@ -0,0 +1,283 @@
+import math
+import mock
+import nengo
+import pytest
+from rig.type_casts import float_to_fix
+import struct
+import tempfile
+
+from nengo_spinnaker.regions.filters import (
+    FilterRegion, FilterRoutingRegion, LowpassFilter, NoneFilter,
+    make_filter_regions)
+from nengo_spinnaker.utils.keyspaces import KeyspaceContainer
+
+s1615 = float_to_fix(True, 32, 15)
+
+
+class TestNoneFilter(object):
+    """Test creating and writing out None filters."""
+    @pytest.mark.parametrize("width, latching", [(5, True), (10, False)])
+    def test_standard(self, width, latching):
+        nf = NoneFilter(width, latching)
+
+        # Pack the filter into some data
+        data = bytearray(nf.size)
+        nf.pack_into(0.001, data)
+
+        # Check the values are sane
+        v1, v2, mask, size = struct.unpack("<4I", data)
+        assert v1 == 0
+        assert v2 == s1615(1.0)
+        assert mask == (0xffffffff if latching else 0x00000000)
+        assert size == width
+
+    @pytest.mark.parametrize("width, latching", [(5, True), (10, False)])
+    def test_from_signal_and_connection(self, latching, width):
+        # Create the mock signal and connection
+        signal = mock.Mock(name="signal", spec_set=["latching"])
+        signal.latching = latching
+
+        connection = mock.Mock(name="connection",
+                               spec_set=["size_out", "synapse"])
+        connection.size_out = width
+        connection.synapse = None
+
+        # Build the filter
+        nf = NoneFilter.from_signal_and_connection(signal, connection)
+        assert NoneFilter(width, latching) == nf
+
+
+class TestLowpassFilter(object):
+    @pytest.mark.parametrize("width, latching, dt, tc",
+                             [(3, False, 0.001, 0.01), (1, True, 0.002, 0.2)])
+    def test_standard(self, width, latching, dt, tc):
+        """Test creating and writing out lowpass filters."""
+        lpf = LowpassFilter(width, latching, tc)
+
+        # Pack the filter into some data
+        data = bytearray(lpf.size)
+        lpf.pack_into(dt, data)
+
+        # Check the values are sane
+        v1, v2, mask, size = struct.unpack("<4I", data)
+        val = math.exp(-dt / tc)
+        assert v1 == s1615(val)
+        assert v2 == s1615(1 - val)
+        assert mask == (0xffffffff if latching else 0x00000000)
+        assert size == width
+
+    @pytest.mark.parametrize("width, latching, tc",
+                             [(3, False, 0.01), (1, True, 0.2)])
+    def test_from_signal_and_connection(self, width, latching, tc):
+        # Create the mock signal and connection
+        signal = mock.Mock(name="signal", spec_set=["latching"])
+        signal.latching = latching
+
+        connection = mock.Mock(name="connection",
+                               spec_set=["size_out", "synapse"])
+        connection.size_out = width
+        connection.synapse = mock.Mock(spec=nengo.Lowpass)
+        connection.synapse.tau = tc
+
+        # Create the filter
+        lpf = LowpassFilter.from_signal_and_connection(signal, connection)
+        assert lpf == LowpassFilter(width, latching, tc)
+
+    def test_eq(self):
+        lpfs = [LowpassFilter(5, True, 0.01),
+                LowpassFilter(5, True, 0.02),
+                LowpassFilter(3, True, 0.01),
+                LowpassFilter(5, False, 0.01)]
+
+        for lpf in lpfs[1:]:
+            assert lpf != lpfs[0]
+
+        lpf = LowpassFilter(5, True, 0.01)
+        assert lpf == lpfs[0]
+
+        nf = NoneFilter(5, True)
+        assert lpf != nf and nf != lpf
+
+
+def test_filter_region():
+    """Test creation of a filter data region."""
+    # Create two filters
+    fs = [LowpassFilter(5, False, 0.1),
+          NoneFilter(3, False)]
+
+    # Create the filter region
+    fr = FilterRegion(fs, dt=0.001)
+
+    # The size should be correct
+    assert fr.sizeof() == 4 + 16 * len(fs)
+
+    # Check that the data is written out correctly
+    fp = tempfile.TemporaryFile()
+    fr.write_to_file(fp)
+    fp.seek(0)
+
+    length, = struct.unpack("<I", fp.read(4))
+    assert length == len(fs)
+
+    expected_data = bytearray(32)
+    fs[0].pack_into(fr.dt, expected_data)
+    fs[1].pack_into(fr.dt, expected_data, 16)
+    assert fp.read() == expected_data
+
+
+def test_filter_routing_region():
+    """Test creation of a filter routing region."""
+    # Define some keyspaces
+    ksc = KeyspaceContainer()
+    ks_a = ksc["nengo"](object=0, connection=3)
+    ks_b = ksc["nengo"](object=127, connection=255, cluster=63, index=15)
+    ksc.assign_fields()
+
+    # Define the filter routes, these map a keyspace to an integer
+    ks_routes = [(ks_a, 12), (ks_b, 17)]
+
+    # Create the region
+    filter_region = FilterRoutingRegion(
+        ks_routes, filter_routing_tag=ksc.filter_routing_tag,
+        index_field="index"
+    )
+
+    # Check that the memory requirement is sane
+    assert filter_region.sizeof() == 4 * (1 + 4*len(ks_routes))
+
+    # Check that the written out data is sensible
+    fp = tempfile.TemporaryFile()
+    filter_region.write_to_file(fp)
+
+    # Check that the data is sensible
+    fp.seek(0)
+    length, = struct.unpack("<I", fp.read(4))
+    assert length == 2
+
+    # Determine valid values
+    valid_mask = ksc["nengo"].get_mask(tag=ksc.filter_routing_tag)
+    valid_d_mask = ksc["nengo"].get_mask(field="index")
+
+    for _ in range(len(ks_routes)):
+        key, mask, i, d_mask = struct.unpack("<4I", fp.read(16))
+        assert mask == valid_mask, hex(mask) + " != " + hex(valid_mask)
+        assert d_mask == valid_d_mask
+
+        for ks, j in ks_routes:
+            if key == ks.get_value(tag=ksc.filter_routing_tag):
+                assert i == j
+                break
+        else:
+            assert False, "Unexpected key " + hex(key)
+
+
+class TestMakeFilterRegions(object):
+    """Test the helper for constructing these regions."""
+    @pytest.mark.parametrize("minimise", [True, False])
+    def test_equivalent_filters(self, minimise):
+        """Test construction of filter regions from signals and keyspaces."""
+        # Create two keyspaces, two signals and two connections with equivalent
+        # synapses.
+        ks_a = mock.Mock(name="Keyspace[A]")
+        signal_a = mock.Mock(name="Signal[A]")
+        signal_a.keyspace, signal_a.latching = ks_a, False
+
+        ks_b = mock.Mock(name="Keyspace[B]")
+        signal_b = mock.Mock(name="Signal[B]")
+        signal_b.keyspace, signal_b.latching = ks_b, False
+
+        conn_a = mock.Mock(name="Connection[A]")
+        conn_a.size_out = 3
+        conn_a.synapse = nengo.Lowpass(0.01)
+
+        conn_b = mock.Mock(name="Connection[B]")
+        conn_b.size_out = 3
+        conn_b.synapse = nengo.Lowpass(0.01)
+
+        # Create the type of dictionary that is expected as input
+        signals_connections = {
+            signal_a: [conn_a],
+            signal_b: [conn_b],
+        }
+
+        # Create the regions, with minimisation
+        filter_region, routing_region = make_filter_regions(
+            signals_connections, 0.001,
+            minimise=minimise,
+            filter_routing_tag="spam",
+            index_field="eggs"
+        )
+
+        # Check that the filter region is as expected
+        assert filter_region.dt == 0.001
+
+        if minimise:
+            assert len(filter_region.filters) == 1
+            assert filter_region.filters[0] == LowpassFilter(3, False, 0.01)
+        else:
+            assert len(filter_region.filters) == 2
+            assert filter_region.filters[0] == LowpassFilter(3, False, 0.01)
+            assert filter_region.filters[1] == LowpassFilter(3, False, 0.01)
+
+        # Check that the routing region is as expected
+        assert routing_region.filter_routing_tag == "spam"
+        assert routing_region.index_field == "eggs"
+        if minimise:
+            assert (ks_a, 0) in routing_region.keyspace_routes
+            assert (ks_b, 0) in routing_region.keyspace_routes
+        else:
+            if (ks_a, 0) in routing_region.keyspace_routes:
+                assert (ks_b, 1) in routing_region.keyspace_routes
+            else:
+                assert (ks_b, 0) in routing_region.keyspace_routes
+
+    def test_different_filters(self):
+        """Test construction of filter regions from signals and keyspaces."""
+        # Create two keyspaces, two signals and two connections with equivalent
+        # synapses.
+        ks_a = mock.Mock(name="Keyspace[A]")
+        signal_a = mock.Mock(name="Signal[A]")
+        signal_a.keyspace, signal_a.latching = ks_a, False
+
+        ks_b = mock.Mock(name="Keyspace[B]")
+        signal_b = mock.Mock(name="Signal[B]")
+        signal_b.keyspace, signal_b.latching = ks_b, False
+
+        conn_a = mock.Mock(name="Connection[A]")
+        conn_a.size_out = 3
+        conn_a.synapse = nengo.Lowpass(0.01)
+
+        conn_b = mock.Mock(name="Connection[B]")
+        conn_b.size_out = 3
+        conn_b.synapse = None
+
+        # Create the type of dictionary that is expected as input
+        signals_connections = {
+            signal_a: [conn_a],
+            signal_b: [conn_b],
+        }
+
+        # Create the regions, with minimisation
+        filter_region, routing_region = make_filter_regions(
+            signals_connections, 0.001,
+            minimise=True,  # Shouldn't achieve anything
+            filter_routing_tag="spam",
+            index_field="eggs"
+        )
+
+        # Check that the filter region is as expected
+        assert filter_region.dt == 0.001
+        assert len(filter_region.filters) == 2
+
+        for f in filter_region.filters:
+            assert (f == LowpassFilter(3, False, 0.01) or
+                    f == NoneFilter(3, False))
+
+        # Check that the routing region is as expected
+        assert routing_region.filter_routing_tag == "spam"
+
+        assert routing_region.index_field == "eggs"
+        if (ks_a, 0) in routing_region.keyspace_routes:
+            assert (ks_b, 1) in routing_region.keyspace_routes
+        else:
+            assert (ks_b, 0) in routing_region.keyspace_routes
