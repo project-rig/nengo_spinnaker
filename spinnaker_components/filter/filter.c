4040ca0d828ad8fef4bed69f94b36daac01e4a72
Andrew Mundy <andrew.mundy@ieee.org>
2016-11-01 10:54:33 +0000

    Use a queue to speed up packet processing

--- spinnaker_components/filter/filter.c
+++ spinnaker_components/filter/filter.c
@@ -29,6 +29,7 @@
 #include "fixed_point.h"
 #include "input_filtering.h"
 #include "common-impl.h"
+#include "packet_queue.h"
 
 /*****************************************************************************/
 // Global variables
@@ -45,6 +46,73 @@ static filter_parameters_t params;
 static if_collection_t filters;  // Locally applied filters
 static value_t *transform;       // Transform matrix
 uint32_t *keys;                  // Multicast keys
+
+static packet_queue_t packets;  // Queued multicast packets
+static bool queue_processing;   // Indicate if the queue is being handled
+static unsigned int queue_overflows;
+/*****************************************************************************/
+
+/*****************************************************************************/
+// Multicast packet handling
+void multicast_packet_payload(uint key, uint payload)
+{
+  // Queue the packet for later processing, if no processing is scheduled then
+  // trigger the queue processor.
+  if (packet_queue_push(&packets, key, payload))
+  {
+    if (!queue_processing)
+    {
+      spin1_trigger_user_event(0, 0);
+      queue_processing = true;
+    }
+  }
+  else
+  {
+    // The packet couldn't be included in the queue, thus it was essentially
+    // dropped.
+    queue_overflows++;
+  }
+}
+
+void process_queue()
+{
+  // Continuously remove packets from the queue and include them in filters
+  while (packet_queue_not_empty(&packets))
+  {
+    // Pop a packet from the queue (critical section)
+    packet_t packet;
+    uint cpsr = spin1_fiq_disable();
+    bool packet_is_valid = packet_queue_pop(&packets, &packet);
+    spin1_mode_restore(cpsr);
+
+    // Process the received packet
+    if (packet_is_valid)
+    {
+      uint32_t key = packet.key;
+      uint32_t payload = packet.payload;
+
+      input_filtering_input_with_dimension_offset(
+        &filters, key, payload,
+        params.input_offset,   // Offset for all packets
+        params.input_size - 1  // Max expected dimension
+      );
+    }
+    else
+    {
+      io_printf(IO_BUF, "Popped packet from empty queue.\n");
+      rt_error(RTE_ABORT);
+    }
+  }
+  queue_processing = false;
+}
+
+void user_event(uint arg0, uint arg1)
+{
+  use(arg0);
+  use(arg1);
+
+  process_queue();
+}
 /*****************************************************************************/
 
 /*****************************************************************************/
@@ -57,6 +125,9 @@ void timer_tick(uint ticks, uint arg1)
     spin1_exit(0);
     return;
   }
+
+  // Process any remaining unprocessed packets
+  process_queue();
 
   // Update the filters
   input_filtering_step(&filters);
@@ -78,18 +149,6 @@ void timer_tick(uint ticks, uint arg1)
     {
     }
   }
-}
-/*****************************************************************************/
-
-/*****************************************************************************/
-// Multicast packet handling
-void multicast_packet_payload(uint key, uint payload)
-{
-  input_filtering_input_with_dimension_offset(
-    &filters, key, payload,
-    params.input_offset,   // Offset for all packets
-    params.input_size - 1  // Max expected dimension
-  );
 }
 /*****************************************************************************/
 
@@ -118,10 +177,16 @@ void c_main(void)
   input_filtering_get_routes(&filters, region_start(4, address));
   input_filtering_initialise_output(&filters, params.input_size);
 
+  // Multicast packet queue
+  queue_processing = false;
+  packet_queue_init(&packets, 1024);
+  queue_overflows = 0;
+
   // Register callbacks
   spin1_set_timer_tick(params.machine_timestep);
   spin1_callback_on(MCPL_PACKET_RECEIVED, multicast_packet_payload, -1);
   spin1_callback_on(TIMER_TICK, timer_tick, 1);
+  spin1_callback_on(USER_EVENT, user_event, 1);
 
   while (true)
   {
@@ -130,6 +195,13 @@ void c_main(void)
 
     // Determine how long to simulate for
     config_get_n_ticks();
+
+    // Check on the status of the packet queue
+    if (queue_overflows)
+    {
+      io_printf(IO_BUF, "Queue overflows = %u\n", queue_overflows);
+      rt_error(RTE_ABORT);
+    }
 
     // Perform the simulation
     spin1_start(SYNC_WAIT);
--- spinnaker_components/value_sink/value_sink.c
+++ spinnaker_components/value_sink/value_sink.c
@@ -2,6 +2,7 @@
 #include "common-typedefs.h"
 #include "common-impl.h"
 #include "input_filtering.h"
+#include "packet_queue.h"
 
 typedef struct _region_system_t
 {
@@ -15,6 +16,70 @@ address_t rec_start, rec_curr;
 
 if_collection_t filters;
 
+static packet_queue_t packets;  // Queued multicast packets
+static bool queue_processing;   // Indicate if the queue is being handled
+static unsigned int queue_overflows;
+
+void mcpl_callback(uint key, uint payload)
+{
+  // Queue the packet for later processing, if no processing is scheduled then
+  // trigger the queue processor.
+  if (packet_queue_push(&packets, key, payload))
+  {
+    if (!queue_processing)
+    {
+      spin1_trigger_user_event(0, 0);
+      queue_processing = true;
+    }
+  }
+  else
+  {
+    // The packet couldn't be included in the queue, thus it was essentially
+    // dropped.
+    queue_overflows++;
+  }
+}
+
+void process_queue()
+{
+  // Continuously remove packets from the queue and include them in filters
+  while (packet_queue_not_empty(&packets))
+  {
+    // Pop a packet from the queue (critical section)
+    packet_t packet;
+    uint cpsr = spin1_fiq_disable();
+    bool packet_is_valid = packet_queue_pop(&packets, &packet);
+    spin1_mode_restore(cpsr);
+
+    // Process the received packet
+    if (packet_is_valid)
+    {
+      uint32_t key = packet.key;
+      uint32_t payload = packet.payload;
+
+      input_filtering_input_with_dimension_offset(
+        &filters, key, payload,
+        params.input_offset,   // Offset for all packets
+        params.input_size - 1  // Max expected dimension
+      );
+    }
+    else
+    {
+      io_printf(IO_BUF, "Popped packet from empty queue.\n");
+      rt_error(RTE_ABORT);
+    }
+  }
+  queue_processing = false;
+}
+
+void user_event(uint arg0, uint arg1)
+{
+  use(arg0);
+  use(arg1);
+
+  process_queue();
+}
+
 void sink_update(uint ticks, uint arg1)
 {
   use(arg1);
@@ -24,19 +89,13 @@ void sink_update(uint ticks, uint arg1)
     return;
   }
 
+  // Process any remaining unprocessed packets
+  process_queue();
+
   // Filter inputs, write the latest value to SRAM
   input_filtering_step(&filters);
   spin1_memcpy(rec_curr, filters.output, params.input_size * sizeof(value_t));
   rec_curr = &rec_curr[params.input_size];
-}
-
-void mcpl_callback(uint key, uint payload)
-{
-  input_filtering_input_with_dimension_offset(
-    &filters, key, payload,
-    params.input_offset,   // Offset for all packets
-    params.input_size - 1  // Max expected dimension
-  );
 }
 
 void c_main(void)
@@ -54,10 +113,16 @@ void c_main(void)
   // Retrieve the recording region
   rec_start = region_start(15, address);
 
+  // Multicast packet queue
+  queue_processing = false;
+  packet_queue_init(&packets, 1024);
+  queue_overflows = 0;
+
   // Set up callbacks, start
   spin1_set_timer_tick(params.timestep);
-  spin1_callback_on(MCPL_PACKET_RECEIVED, mcpl_callback, -1);
+  spin1_callback_on(MCPL_PACKET_RECEIVED,  mcpl_callback, -1);
   spin1_callback_on(TIMER_TICK, sink_update, 2);
+  spin1_callback_on(USER_EVENT, user_event, 2);
 
   while(true)
   {
@@ -69,6 +134,13 @@ void c_main(void)
 
     // Reset the recording region location
     rec_curr = rec_start;
+
+    // Check on the status of the packet queue
+    if (queue_overflows)
+    {
+      io_printf(IO_BUF, "Queue overflows = %u\n", queue_overflows);
+      rt_error(RTE_ABORT);
+    }
 
     // Perform the simulation
     spin1_start(SYNC_WAIT);
