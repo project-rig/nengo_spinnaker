31dcc4bb3396a3b81f54007a0a1574fc4ca01811
Terry Stewart <tcstewar@uwaterloo.ca>
2017-05-03 16:07:34 -0400

    Added experimental async mode

--- nengo_spinnaker/node_io/ethernet.py
+++ nengo_spinnaker/node_io/ethernet.py
@@ -158,26 +158,29 @@ class EthernetThread(threading.Thread):
 
     def run(self):
         while not self.halt:
-            # Read as many packets from the socket as we can
-            while True:
-                try:
-                    data = self.in_sock.recv(512)
-                except IOError:
-                    break  # No more to read
+            self.step()
 
-                # Unpack the data, and store it as the input for the
-                # appropriate Node.
-                packet = SCPPacket.from_bytestring(data)
-                values = tp.fix_to_np(
-                    np.frombuffer(packet.data, dtype=np.int32)
-                )
+    def step(self):
+        # Read as many packets from the socket as we can
+        while True:
+            try:
+                data = self.in_sock.recv(512)
+            except IOError:
+                break  # No more to read
 
-                # Get the Node
-                node = self.handler._node_incoming[(packet.src_x,
-                                                    packet.src_y,
-                                                    packet.src_cpu)]
-                with self.handler.node_input_lock:
-                    self.handler.node_input[node] = values[:]
+            # Unpack the data, and store it as the input for the
+            # appropriate Node.
+            packet = SCPPacket.from_bytestring(data)
+            values = tp.fix_to_np(
+                np.frombuffer(packet.data, dtype=np.int32)
+            )
+
+            # Get the Node
+            node = self.handler._node_incoming[(packet.src_x,
+                                                packet.src_y,
+                                                packet.src_cpu)]
+            with self.handler.node_input_lock:
+                self.handler.node_input[node] = values[:]
 
     def stop(self):
         """Stop the thread from running."""
